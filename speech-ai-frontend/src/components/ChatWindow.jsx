d184dbfec9f2999743cbb5d5104ab5e506c8a7c4
Harshit Mourya <mouryaharshit03@gmail.com>
2025-06-04 19:56:38 +0530

    Add indicator in chat window while AI is typing

--- speech-ai-frontend/src/components/ChatWindow.jsx
+++ speech-ai-frontend/src/components/ChatWindow.jsx
@@ -6,8 +6,7 @@ import "./ChatWindow.css";
 import Loader from "./Loader";
 
 const ChatWindow = () => {
-  const { messages, setMessages, messagesLoading } = useChat();
-  // const [userName, setUserName] = useState("");
+  const { messages, messagesLoading, loading } = useChat();
   const { isLoggedIn, user, isAuthLoading } = useAuth();
   const chatEndRef = useRef(null);
 
@@ -25,14 +24,24 @@ const ChatWindow = () => {
           Hey {isLoggedIn ? user.name : "there"}, how can I assist you today?
         </p>
       ) : (
-        messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`chat-message ${msg.sender === "user" ? "user" : "ai"}`}
-          >
-            {msg.text}
-          </div>
-        ))
+        <>
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`chat-message ${
+                msg.sender === "user" ? "user" : "ai"
+              }`}
+            >
+              {msg.text}
+            </div>
+          ))}
+          {loading && (
+            <div className="chat-message ai typing-indicator">
+              <i className="fa-solid fa-robot"></i>
+              Generating response...
+            </div>
+          )}
+        </>
       )}
       <div ref={chatEndRef} />
     </div>
